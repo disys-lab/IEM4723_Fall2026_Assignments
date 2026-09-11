878739c8c2bbf4cd88c8dc4cceab2c6da22efc40
elihamilton-create <eli.hamilton@okstate.edu>
2026-09-11 16:05:28 -0500

    Finalize Assignment

--- docker_commands.sh
+++ docker_commands.sh
@@ -6,6 +6,7 @@ docker tag linear-regression-app:latest elijah822/linear-regression-app:latest
 docker push elijah822/linear-regression-app:latest
 
 # Question 2
+docker build -t volume-mounting-app .
 docker run --rm --volume "%cd%\data:/home/iem4723/data" volume-mounting-app
 
 # Question 3
@@ -14,4 +15,5 @@ docker run --rm logistic_regression_layered:latest ls -l /home/iem4723/
 docker run --rm --volume "%cd%\data:/home/iem4723/data" logistic_regression_layered:latest
 
 # Question 4
+docker build -t simple_file_server .
 docker run --rm --publish 3333:8000 simple_file_server
