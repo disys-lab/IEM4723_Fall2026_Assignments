844efe809f94d632179154865670a098147d3814
elihamilton-create <eli.hamilton@okstate.edu>
2026-09-11 15:21:32 -0500

    Complete Question 2

--- docker_commands.sh
+++ docker_commands.sh
@@ -6,7 +6,7 @@ docker tag linear-regression-app:latest elijah822/linear-regression-app:latest
 docker push elijah822/linear-regression-app:latest
 
 # Question 2
-
+docker run --rm --volume "%cd%\data:/home/iem4723/data" volume-mounting-app
 
 # Question 3
 
