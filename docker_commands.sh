589a806088e00020e86e2971b089e2ca72c80b1f
elihamilton-create <eli.hamilton@okstate.edu>
2026-09-11 15:04:54 -0500

    Complete Question 1

---
+++ docker_commands.sh
@@ -0,0 +1,14 @@
+# Question 1
+docker build -t linear-regression-app .
+docker tag linear-regression-app:latest linear-regression-app:v0.1.1
+docker tag linear-regression-app:latest linear-regression-app:v0.1.2
+docker tag linear-regression-app:latest elijah822/linear-regression-app:latest
+docker push elijah822/linear-regression-app:latest
+
+# Question 2
+
+
+# Question 3
+
+
+# Question 4
