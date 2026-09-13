59713630170f9b2b6f325951b0ecf324369c3a60
jacobflud12 <jacobflud12@gmail.com>
2026-09-13 18:41:04 -0500

    Complete question 4 port forwarding

--- docker commands.sh
+++ docker commands.sh
@@ -12,3 +12,7 @@ docker run --name volume_regression --volume "C:/Users/bubba/Documents/GitHub/IE
 # question 3
 docker build -t logistic_regression_layered:latest .
 docker run --name logistic_regression_container --volume "C:/Users/bubba/Documents/GitHub/IEM4723_Fall2026_Assignments/assignment1/layering/data:/home/iem4723/data" logistic_regression_layered:latest 
+
+# question 4
+docker build -t simple_file_server:latest .
+docker run --name simple_file_server --publish 3333:8000 -d simple_file_server:latest
