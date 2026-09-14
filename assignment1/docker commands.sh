f0cceb01c6c5a33cb5ba30db381363c9d7dcbb48
daytonmccall <dayton.mccall@okstate.edu>
2026-09-14 17:24:10 -0500

    Question 1: Build and Tag

---
+++ assignment1/docker commands.sh
@@ -0,0 +1,16 @@
+# Question 1
+docker build -t linear-regression-app .
+docker tag linear-regression-app linear-regression-app:v0.1.1
+docker tag linear-regression-app linear-regression-app:v0.1.2
+docker tag linear-regression-app:latest daytonmccall/linear-regression-app:latest
+docker push daytonmccall/linear-regression-app:latest
+
+
+# Question 2
+
+
+# Question 3
+
+
+# Question 4
+
