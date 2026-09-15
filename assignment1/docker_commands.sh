8ced213545a3c38ef5dc6c498fc16d0eaf16528a
emehen <elanor.mehen@okstate.edu>
2026-09-15 17:38:15 -0500

    Question 1: Build and Tag
    Built the linear-regression-app container, tagged versions v0.1.1 and v0.1.2, namespaced the image under my username - emehen, and pushed the latest image to Docker Hub

--- assignment1/docker_commands.sh
+++ assignment1/docker_commands.sh
@@ -1,5 +1,9 @@
 # Question 1
-
+docker build -t linear-regression-app .
+docker tag linear-regression-app linear-regression-app:v0.1.1
+docker tag linear-regression-app linear-regression-app:v0.1.2
+docker tag linear-regression-app emehen/linear-regression-app:latest
+docker push emehen/linear-regression-app:latest
 
 # Question 2
 
