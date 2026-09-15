765ce9278a3fd11a2a288f745137978d7a688d9d
connornrice <connor.n.rice@gmail.com>
2026-09-15 17:30:23 -0500

    #Question1

---
+++ assignment1/docker_commands.sh
@@ -0,0 +1,7 @@
+#Question 1
+··• cd /run/media/poisonoftheclockwork/3C02D63C02D5FABA/School/S7Fall2026/"InfoSys Design"/Assignments/Assignment1/IEM4723_Fall2026_Assignments/assignment1/regression_container
+··• pwd
+··• docker build -t linear-regression-app .
+··• docker tag linear-regression-app linear-regression-app:v0.1.1
+··• docker tag linear-regression-app linear-regression-app:v0.1.2
+··• docker tag linear-regression-app connornrice/linear-regression-app:latest
