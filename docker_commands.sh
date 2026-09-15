2ed2ddd5cd0cdd84bfc7a093570bbe87f46ee38f
mkuhn23 <kuhnmlt@gmail.com>
2026-09-15 00:08:13 -0500

    Complete Q3 Docker Layering

--- docker_commands.sh
+++ docker_commands.sh
@@ -5,3 +5,10 @@ docker tag linear-regression-app:latest linear-regression-app:v0.1.2
 docker images linear-regression-app
 docker tag linear-regression-app:latest mkuhn23/linear-regression-app:latest
 docker push mkuhn23/linear-regression-app:latest
+
+# Question 2
+docker build -t volume-mounting-app .
+docker run --rm --volume "${PWD}\data:/home/iem4723/data" volume-mounting-app
+
+# Question 3
+docker build -t logistic_regression_layered:latest .
