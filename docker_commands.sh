1cb4fdbbd644ce1c17688bc9f746a4ddd77833d2
Luke Diener <luke.diener@okstate.edu>
2026-09-15 15:07:30 -0500

    Q2 build and run commands added

--- docker_commands.sh
+++ docker_commands.sh
@@ -1,14 +1,18 @@
 # Question 1
-cd regression_containter #navigates to current directory
-docker build -t linear-regression-app .  #builds and names a new docker image
-docker tag linear-regression-app linear-regression-app:v0.1.1 #tags another version of the image
-docker tag linear-regression-app linear-regression-app:v0.1.2 #tags another version of the image
-docker tag linear-regression-app lukediener/linear-regression-app:latest #retags the image with my username
-docker push lukediener/linear-regression-app:latest #pushes to remote Docker GitHub repo
+    docker build -t linear-regression-app .  #builds and names a new docker image
+    docker tag linear-regression-app linear-regression-app:v0.1.1 #tags another version of the image
+    docker tag linear-regression-app linear-regression-app:v0.1.2 #tags another version of the image
+    docker tag linear-regression-app lukediener/linear-regression-app:latest #retags the image with my username
+    docker push lukediener/linear-regression-app:latest #pushes to remote Docker GitHub repo
 
 # Question 2
+    #build the volume mounting container image
+    docker build -t volume_app:latest .
 
+    #run the container with volume mounting to sync host and container data
+    docker run --name volume_test --volume C:\Users\luked\Documents\GitHub\IEM4723_Fall2026_Assignments\assignment1\volume-mounting\data\:/home/iem4723/data/ -d volume_app:latest
 
 # Question 3
+
 
 # Question 4
