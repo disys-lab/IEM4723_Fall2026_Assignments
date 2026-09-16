fe56bbf909d54caefebfb426a7c6738fc066332c
MicahBullard <micah.bullard@okstate.edu>
2026-09-15 22:26:14 -0400

    Commit 1
    Commit for Q1

---
+++ docker_commands.sh
@@ -0,0 +1,9 @@
+# Question 1
+
+docker build -t linear-regression-app .
+docker tag linear-regression-app linear-regression-app:v0.1.1
+docker tag linear-regression-app linear-regression-app:v0.1.2
+docker tag linear-regression-app micahjbullard/linear-regression-app:latest
+docker push micahjbullard/linear-regression-app:latest
+
+# Question 2
