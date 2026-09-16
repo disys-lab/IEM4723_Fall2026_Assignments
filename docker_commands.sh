81076bcb48452d67e54f0bc37d9bb53008f5dee6
morrow-e <107022775+morrow-e@users.noreply.github.com>
2026-09-16 00:16:57 -0500

    Add Docker commands for linear regression app
    Add commands to initialize git and build Docker image for linear regression app.

--- docker_commands.sh
+++ docker_commands.sh
@@ -1,1 +1,7 @@
-
+# Question 1
+cd "C:\Users\18179\Documents\GitHub\IEM4723_Fall2026_Assignments\assignment1\regression_container"; git init
+docker build -t linear-regression-app .
+docker tag linear-regression-app:v0.1.1
+docker tag linear-regression-app:v0.1.2
+docker tag linear-regression-app morrowe/linear-regression-app:latest
+docker push morrowe/linear-regression-app:latest
