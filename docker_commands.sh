14f4d9a1bcd0667a83ce809050bd7118839e8e23
Zicotix <chronoduty707@gmail.com>
2026-09-15 21:48:25 -0500

    Built and Ran RV Q2
    Built and ran regression-volume for Question 2. Regression_coefficients.csv created in turn.

--- docker_commands.sh
+++ docker_commands.sh
@@ -6,3 +6,8 @@ docker build -t linear-regression-app:v0.1.1 -t linear-regression-app:v0.1.2 .
 docker tag linear-regression-app:v0.1.2 zicotix/linear-regression-app:latest
 docker push zicotix/linear-regression-app:latest 
 
+#Question 2
+
+docker build -t regression-volume .
+docker run --volume "$(pwd)\data:/home/iem4723/data" regression-volume
+
