2141145662e21acc7feb896ffcd0eb5631366db5
morrow-e <107022775+morrow-e@users.noreply.github.com>
2026-09-16 02:17:28 -0500

    Add commands for simple file server setup

--- docker_commands.sh
+++ docker_commands.sh
@@ -12,3 +12,7 @@ docker run -v C:\path\to\data:/app/data morrowe/linear-regression-app:latest
 # Question 3
 docker build -t logistic_regression_layered:latest .
 docker run -v C:\Users\18179\Documents\GitHub\IEM4723_Fall2026_Assignments\assignment1\layering\data:/home/iem4723/data logistic_regression_layered:latest
+
+# Question 4
+docker build -t simple_file_server .
+docker run -p 3333:8000 -v C:\Users\18179\Documents\GitHub\IEM4723_Fall2026_Assignments\assignment1\layering\data:/home/iem4723/data logistic_regression_layered:latest
