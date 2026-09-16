7b380898cd1ff3e80f1c6136c94efe09aa2c56f4
lkokal <lkokal@okstate.edu>
2026-09-15 23:17:52 -0500

    Update docker_commands.sh
    question 2 part 1 changes

--- docker_commands.sh
+++ docker_commands.sh
@@ -7,3 +7,7 @@ docker tag linear-regression-app linear regression-app:v0.1.2
 docker tag linear-reggression-app lkokal/linear-regression-app:latest
 
 docker push lkokal/linear-regression-app:latest
+
+
+#Question 2
+docker run --rm --volume "C:\Users\laura\OneDrive\Documents\Github\IEM4723_Fall2026_Assignments\assignment1\volume-mounting\data:/home/iem4723/data" linear-regression-volume
