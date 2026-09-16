5779f5efe65db5c0567d322f41246ee2fea7ae2f
killianbarnes2 <killianbarnes2@gmail.com>
2026-09-15 23:11:44 -0500

    Add Question 4 port listening

--- assignment1/docker_commands.sh
+++ assignment1/docker_commands.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#Killian Barnes
 
 # Question 1
 docker build -t linear-regression-app .
@@ -9,8 +9,13 @@ docker tag linear-regression-app:latest killianbarnes2/linear-regression-app:lat
 docker push killianbarnes2/linear-regression-app:latest
 
 # Question 2
+docker run -t linear-regression-app-volume
 docker run --rm -v "$(pwd)/data:/home/iem4723/data" linear-regression-app-volume
 
 # Question 3
 docker build -t logistic_regression_layered:latest .
 docker run --rm -v "$(pwd)/data:/home/iem4723/data" logistic_regression_layered:latest
+
+# Question 4
+docker build -t simple_file_search:latest .
+docker run --publish 3333:8000 simple_file_search:latest
