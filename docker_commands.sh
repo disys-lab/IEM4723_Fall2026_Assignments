7e42409d20d17bfd02a8aeb9f897b4e993ff1afe
lkokal <lkokal@okstate.edu>
2026-09-15 20:01:32 -0500

    Update docker_commands.sh
    question 1 update

--- docker_commands.sh
+++ docker_commands.sh
@@ -1,1 +1,9 @@
 #Question 1
+docker build -t linear-regrssion-app . 
+
+docker tag linear-regression-app linear regression-app:v0.1.1
+docker tag linear-regression-app linear regression-app:v0.1.2
+
+docker tag linear-reggression-app lkokal/linear-regression-app:latest
+
+docker push lkokal/linear-regression-app:latest
