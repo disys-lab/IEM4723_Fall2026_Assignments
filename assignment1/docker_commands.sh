4da6582f7aad2708733137d9a131450595a58ace
killianbarnes2 <killianbarnes2@gmail.com>
2026-09-15 22:19:25 -0500

    Add Question 2 volume mounting

--- assignment1/docker_commands.sh
+++ assignment1/docker_commands.sh
@@ -7,3 +7,7 @@ docker tag linear-regression-app:latest linear-regression-app:v0.1.2
 docker login
 docker tag linear-regression-app:latest killianbarnes2/linear-regression-app:latest
 docker push killianbarnes2/linear-regression-app:latest
+
+# Question 2
+docker run --rm -v "$(pwd)/data:/home/iem4723/data" linear-regression-app-volume
+ls -l data
