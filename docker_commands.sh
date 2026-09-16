8e874d4328ab1a953985dfee60299d84a21a6a32
Zicotix <chronoduty707@gmail.com>
2026-09-15 21:38:40 -0500

    Question 1 Correction
    Corrected docker username.

--- docker_commands.sh
+++ docker_commands.sh
@@ -3,6 +3,6 @@
 #Question 1                                     
 
 docker build -t linear-regression-app:v0.1.1 -t linear-regression-app:v0.1.2 .
-docker tag linear-regression-app:v0.1.2 ttvzicotix/linear-regression-app:latest
+docker tag linear-regression-app:v0.1.2 zicotix/linear-regression-app:latest
 docker push zicotix/linear-regression-app:latest 
 
