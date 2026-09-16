de27876f711ed7cabcd6ff14269dbcf22a9ba4b8
nadiaphan <nadia.phan@okstate.edu>
2026-09-15 20:11:15 -0500

    Completed Question 4 port forwarding

--- assignment1/docker_commands.sh
+++ assignment1/docker_commands.sh
@@ -11,3 +11,6 @@ docker run --volume "${PWD}\data:/home/iem4723/data" volume-mounting-app
 
 #question 3
 docker run --rm -v "${PWD}\data:/home/iem4723/data" logistic_regression_layered:latest
+
+#question 4
+docker run --rm -p 3333:8000 simple-file-server
