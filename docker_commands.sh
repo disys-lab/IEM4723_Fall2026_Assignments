e21cf5ba596c34e80479dbf697b073e697b8d4e5
Zicotix <chronoduty707@gmail.com>
2026-09-15 22:22:21 -0500

    Built and ran Q3
    Built and ran LRL image using base image from Q1 and volume mapping sequence from Q2 for Question 3, logistic_regression_coefficients.csv created in turn. Also added directory commands between questions.

--- docker_commands.sh
+++ docker_commands.sh
@@ -1,13 +1,21 @@
 #Andrew Gungoll
 
-#Question 1                                     
-
+#Question 1  
+cd assignment1                                   
+cd regression_container
 docker build -t linear-regression-app:v0.1.1 -t linear-regression-app:v0.1.2 .
 docker tag linear-regression-app:v0.1.2 zicotix/linear-regression-app:latest
 docker push zicotix/linear-regression-app:latest 
+cd ..
 
 #Question 2
-
+cd volume-mounting
 docker build -t regression-volume .
-docker run --volume "$(pwd)\data:/home/iem4723/data" regression-volume
+docker run --volume "$(pwd)/data:/home/iem4723/data" regression-volume
+cd ..
 
+#Question 3
+cd layering
+docker build -t logistic_regression_layered:latest . 
+docker run --volume "$(pwd)/data:/home/iem4723/data" logistic_regression_layered:latest
+cd ..
