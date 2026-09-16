35f64a345d212815ed09179343df0079e6c2b6b4
nadiaphan <nadia.phan@okstate.edu>
2026-09-15 19:44:10 -0500

    Completed Question 2 volume mounting

--- assignment1/docker_commands.sh
+++ assignment1/docker_commands.sh
@@ -4,3 +4,7 @@ docker tag linear-regression-app:latest linear-regression-app:v0.1.1
 docker tag linear-regression-app:latest linear-regression-app:v0.1.2
 docker tag linear-regression-app:latest nadiaphan/linear-regression-app:latest
 docker push nadiaphan/linear-regression-app:latest
+
+#question 2
+docker build -t volume-mounting-app .
+docker run --volume "${PWD}\data:/home/iem4723/data" volume-mounting-app
