9a1f65a3a5d28bf85f0b3e091cd8b6af9336be96
lkokal <lkokal@okstate.edu>
2026-09-15 23:44:51 -0500

    Update docker_commands.sh
    question 3

--- docker_commands.sh
+++ docker_commands.sh
@@ -11,3 +11,8 @@ docker push lkokal/linear-regression-app:latest
 
 #Question 2
 docker run --rm --volume "C:\Users\laura\OneDrive\Documents\Github\IEM4723_Fall2026_Assignments\assignment1\volume-mounting\data:/home/iem4723/data" linear-regression-volume
+
+
+#Question 3
+docker build -t logistic_regression_layered:latest ./layering
+docker run --rm --volume "C:\Users\laura\OneDrive\Documents\GitHub\IEM4723_Fall2026_Assignments\assignment1\layering\data:/home/iem4723/data" logistic_regression_layered:latest
